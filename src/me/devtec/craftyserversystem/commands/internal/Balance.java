9d121d974b56b1c67881ede9f94be1a8c32e11c9
Straikerinos <Straiker123@email.cz>
2023-11-06 19:39:16 +0100

    Modified balance command, added economy command

--- src/me/devtec/craftyserversystem/commands/internal/Balance.java
+++ src/me/devtec/craftyserversystem/commands/internal/Balance.java
@@ -1,5 +1,6 @@
 package me.devtec.craftyserversystem.commands.internal;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
@@ -11,9 +12,9 @@ import org.bukkit.entity.Player;
 import me.devtec.craftyserversystem.API;
 import me.devtec.craftyserversystem.commands.CssCommand;
 import me.devtec.craftyserversystem.placeholders.PlaceholdersExecutor;
-import me.devtec.shared.commands.selectors.Selector;
 import me.devtec.shared.commands.structures.CommandStructure;
 import me.devtec.shared.utility.OfflineCache.Query;
+import me.devtec.theapi.bukkit.BukkitLoader;
 
 public class Balance extends CssCommand {
 
@@ -35,18 +36,6 @@ public class Balance extends CssCommand {
 			msg(sender, "self", PlaceholdersExecutor.i().add("balance", API.get().getEconomyHook().getBalance(sender.getName(), ((Player) sender).getWorld().getName())));
 		}).permission(getPerm("cmd"));
 		// other
-		cmd.selector(Selector.PLAYER, (sender, structure, args) -> {
-			World world = null;
-			if (sender instanceof Player)
-				world = ((Player) sender).getWorld();
-			else if (sender instanceof BlockCommandSender)
-				world = ((BlockCommandSender) sender).getBlock().getWorld();
-			else
-				world = Bukkit.getWorlds().get(0);
-			String targetName = Bukkit.getPlayer(args[0]).getName();
-			msg(sender, "other", PlaceholdersExecutor.i().add("target", targetName).add("balance", API.get().getEconomyHook().getBalance(targetName, world.getName())));
-		}).priority(1).permission(getPerm("other"));
-		// other
 		cmd.argument(null, 1, (sender, structure, args) -> {
 			Query query = me.devtec.shared.API.offlineCache().lookupQuery(args[0]);
 			if (query != null) {
@@ -60,8 +49,13 @@ public class Balance extends CssCommand {
 				msg(sender, "other", PlaceholdersExecutor.i().add("target", query.getName()).add("balance", API.get().getEconomyHook().getBalance(query.getName(), world.getName())));
 			} else
 				msg(sender, "no-account", PlaceholdersExecutor.i().add("target", args[0]));
+		}, (sender, structure, args) -> {
+			List<String> players = new ArrayList<>();
+			players.add("{offlinePlayer}");
+			for (Player player : BukkitLoader.getOnlinePlayers())
+				players.add(player.getName());
+			return players;
 		}).permission(getPerm("other"));
-
 		// register
 		List<String> cmds = getCommands();
 		if (!cmds.isEmpty())
---
+++ src/me/devtec/craftyserversystem/commands/internal/Economy.java
@@ -0,0 +1,130 @@
+package me.devtec.craftyserversystem.commands.internal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.BlockCommandSender;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import me.devtec.craftyserversystem.API;
+import me.devtec.craftyserversystem.commands.CssCommand;
+import me.devtec.craftyserversystem.placeholders.PlaceholdersExecutor;
+import me.devtec.shared.commands.selectors.Selector;
+import me.devtec.shared.commands.structures.CommandStructure;
+import me.devtec.shared.utility.OfflineCache.Query;
+import me.devtec.shared.utility.ParseUtils;
+import me.devtec.theapi.bukkit.BukkitLoader;
+
+public class Economy extends CssCommand {
+
+	@Override
+	public String section() {
+		return "economy";
+	}
+
+	@Override
+	public void register() {
+		if (isRegistered())
+			return;
+
+		CommandStructure<CommandSender> cmd = CommandStructure.create(CommandSender.class, DEFAULT_PERMS_CHECKER, (sender, structure, args) -> {
+			msgUsage(sender, "cmd");
+		}).permission(getPerm("cmd"));
+		// add
+		cmd.argument("add", (sender, structure, args) -> {
+			msgUsage(sender, "add");
+		}).permission(getPerm("add")).argument(null, 1, (sender, structure, args) -> {
+			msgUsage(sender, "set");
+		}, (sender, structure, args) -> {
+			List<String> players = new ArrayList<>();
+			players.add("{offlinePlayer}");
+			for (Player player : BukkitLoader.getOnlinePlayers())
+				players.add(player.getName());
+			return players;
+		}).selector(Selector.NUMBER, (sender, structure, args) -> {
+			double value = ParseUtils.getDouble(args[2]);
+			Query query = me.devtec.shared.API.offlineCache().lookupQuery(args[1]);
+			if (query != null) {
+				World world = null;
+				if (sender instanceof Player)
+					world = ((Player) sender).getWorld();
+				else if (sender instanceof BlockCommandSender)
+					world = ((BlockCommandSender) sender).getBlock().getWorld();
+				else
+					world = Bukkit.getWorlds().get(0);
+				API.get().getEconomyHook().deposit(query.getName(), world.getName(), value);
+				msg(sender, "add", PlaceholdersExecutor.i().add("target", query.getName()).add("balance", value));
+			} else
+				msg(sender, "no-account", PlaceholdersExecutor.i().add("target", args[1]));
+		});
+		// remove
+		cmd.argument("remove", (sender, structure, args) -> {
+			msgUsage(sender, "remove");
+		}).permission(getPerm("remove")).argument(null, 1, (sender, structure, args) -> {
+			msgUsage(sender, "set");
+		}, (sender, structure, args) -> {
+			List<String> players = new ArrayList<>();
+			players.add("{offlinePlayer}");
+			for (Player player : BukkitLoader.getOnlinePlayers())
+				players.add(player.getName());
+			return players;
+		}).selector(Selector.NUMBER, (sender, structure, args) -> {
+			double value = ParseUtils.getDouble(args[2]);
+			Query query = me.devtec.shared.API.offlineCache().lookupQuery(args[1]);
+			if (query != null) {
+				World world = null;
+				if (sender instanceof Player)
+					world = ((Player) sender).getWorld();
+				else if (sender instanceof BlockCommandSender)
+					world = ((BlockCommandSender) sender).getBlock().getWorld();
+				else
+					world = Bukkit.getWorlds().get(0);
+				API.get().getEconomyHook().withdraw(query.getName(), world.getName(), value);
+				msg(sender, "remove", PlaceholdersExecutor.i().add("target", query.getName()).add("balance", value));
+			} else
+				msg(sender, "no-account", PlaceholdersExecutor.i().add("target", args[1]));
+		});
+
+		// set
+		cmd.argument("set", (sender, structure, args) -> {
+			msgUsage(sender, "set");
+		}).permission(getPerm("set")).argument(null, 1, (sender, structure, args) -> {
+			msgUsage(sender, "set");
+		}, (sender, structure, args) -> {
+			List<String> players = new ArrayList<>();
+			players.add("{offlinePlayer}");
+			for (Player player : BukkitLoader.getOnlinePlayers())
+				players.add(player.getName());
+			return players;
+		}).selector(Selector.NUMBER, (sender, structure, args) -> {
+			double value = ParseUtils.getDouble(args[2]);
+			Query query = me.devtec.shared.API.offlineCache().lookupQuery(args[1]);
+			if (query != null) {
+				World world = null;
+				if (sender instanceof Player)
+					world = ((Player) sender).getWorld();
+				else if (sender instanceof BlockCommandSender)
+					world = ((BlockCommandSender) sender).getBlock().getWorld();
+				else
+					world = Bukkit.getWorlds().get(0);
+				double currentBalance = API.get().getEconomyHook().getBalance(query.getName(), world.getName());
+				if (currentBalance < 0)
+					currentBalance = 0;
+				if (currentBalance < value)
+					API.get().getEconomyHook().deposit(query.getName(), world.getName(), value - currentBalance);
+				else if (currentBalance != value)
+					API.get().getEconomyHook().withdraw(query.getName(), world.getName(), currentBalance - value);
+				msg(sender, "set", PlaceholdersExecutor.i().add("target", query.getName()).add("balance", value));
+			} else
+				msg(sender, "no-account", PlaceholdersExecutor.i().add("target", args[1]));
+		});
+
+		// register
+		List<String> cmds = getCommands();
+		if (!cmds.isEmpty())
+			this.cmd = addBypassSettings(cmd).build().register(cmds.remove(0), cmds.toArray(new String[0]));
+	}
+}
