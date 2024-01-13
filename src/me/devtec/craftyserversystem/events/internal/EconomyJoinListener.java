9b769bddf921d11e8fb88c5a59c73a182b580eb5
Straikerinos <Straiker123@email.cz>
2024-01-14 00:50:17 +0100

    Fixed creating economy account when switching world or joining server

---
+++ src/me/devtec/craftyserversystem/events/internal/EconomyJoinListener.java
@@ -0,0 +1,44 @@
+package me.devtec.craftyserversystem.events.internal;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import me.devtec.craftyserversystem.API;
+import me.devtec.craftyserversystem.economy.CssEconomyHook;
+import me.devtec.craftyserversystem.events.CssListener;
+import me.devtec.shared.dataholder.Config;
+
+public class EconomyJoinListener implements Listener, CssListener {
+
+	@Override
+	public Config getConfig() {
+		return API.get().getConfigManager().getEconomy();
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return getConfig().getBoolean("enabled");
+	}
+
+	@Override
+	public void reload() {
+
+	}
+
+	@EventHandler
+	public void onLogin(PlayerJoinEvent e) {
+		if (API.get().getEconomyHook() instanceof CssEconomyHook)
+			if (!((CssEconomyHook) API.get().getEconomyHook()).economy.hasAccount(e.getPlayer().getName(), e.getPlayer().getWorld().getName()))
+				((CssEconomyHook) API.get().getEconomyHook()).economy.createPlayerAccount(e.getPlayer().getName(), e.getPlayer().getWorld().getName());
+	}
+
+	@EventHandler
+	public void onWorldChange(PlayerChangedWorldEvent e) {
+		if (API.get().getEconomyHook() instanceof CssEconomyHook)
+			if (((CssEconomyHook) API.get().getEconomyHook()).economy.isEnabledPerWorldEconomy()
+					&& !((CssEconomyHook) API.get().getEconomyHook()).economy.hasAccount(e.getPlayer().getName(), e.getPlayer().getWorld().getName()))
+				((CssEconomyHook) API.get().getEconomyHook()).economy.createPlayerAccount(e.getPlayer().getName(), e.getPlayer().getWorld().getName());
+	}
+}
