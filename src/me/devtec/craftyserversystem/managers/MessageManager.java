7639a5f6271902038f840359ba29258e8eac2bd0
Straikerinos <Straiker123@email.cz>
2024-01-14 00:48:12 +0100

    Added to the chat new placeholders

--- src/me/devtec/craftyserversystem/managers/MessageManager.java
+++ src/me/devtec/craftyserversystem/managers/MessageManager.java
@@ -1,29 +1,50 @@
 package me.devtec.craftyserversystem.managers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.devtec.craftyserversystem.API;
 import me.devtec.craftyserversystem.Loader;
+import me.devtec.craftyserversystem.events.internal.PreCommandListener;
 import me.devtec.craftyserversystem.placeholders.PlaceholdersExecutor;
+import me.devtec.shared.components.ClickEvent;
+import me.devtec.shared.components.Component;
 import me.devtec.shared.components.ComponentAPI;
+import me.devtec.shared.components.ComponentItem;
+import me.devtec.shared.components.HoverEvent;
 import me.devtec.shared.dataholder.Config;
 import me.devtec.shared.dataholder.StringContainer;
 import me.devtec.shared.json.custom.CustomJsonWriter;
 import me.devtec.shared.scheduler.Tasker;
+import me.devtec.shared.utility.ColorUtils;
 import me.devtec.theapi.bukkit.BukkitLoader;
+import me.devtec.theapi.bukkit.game.ItemMaker;
+import me.devtec.theapi.bukkit.gui.GUI;
 import me.devtec.theapi.bukkit.nms.NmsProvider.ChatType;
+import me.devtec.theapi.bukkit.xseries.XMaterial;
 
 public class MessageManager {
+
+	private static ItemStack EMPTY;
+	static {
+		EMPTY = ItemMaker.of(XMaterial.BLACK_STAINED_GLASS_PANE).displayName("&f").build();
+	}
 
 	private final Logger logger = JavaPlugin.getPlugin(Loader.class).getLogger();
 
@@ -33,6 +54,7 @@ public class MessageManager {
 		PlaceholdersExecutor executor;
 		CommandSender[] receivers;
 		CompletableFuture<String> result;
+		Player owner;
 
 		public Action(Config config, String path, PlaceholdersExecutor executor, CommandSender[] receivers) {
 			this.config = config;
@@ -41,12 +63,13 @@ public class MessageManager {
 			this.receivers = receivers;
 		}
 
-		public Action(Config config, String path, PlaceholdersExecutor executor, CommandSender[] receivers, CompletableFuture<String> result) {
+		public Action(Config config, String path, PlaceholdersExecutor executor, CommandSender[] receivers, CompletableFuture<String> result, Player owner) {
 			this.config = config;
 			this.path = path;
 			this.executor = executor;
 			this.receivers = receivers;
 			this.result = result;
+			this.owner = owner;
 		}
 
 		public void process() {
@@ -57,26 +80,39 @@ public class MessageManager {
 					return;
 				}
 
-				Object chatBase;
+				List<Component> components;
+				String inString;
 				boolean collection = false;
 				if (config.isJson(path) && (config.get(path) instanceof Collection || config.get(path) instanceof Map)) {
 					if (config.get(path) instanceof Collection && ((Collection<?>) config.get(path)).isEmpty() || config.get(path) instanceof Map && ((Map<?, ?>) config.get(path)).isEmpty()) {
 						result.complete(null);
 						return;
 					}
-					chatBase = BukkitLoader.getNmsProvider().chatBase(CustomJsonWriter.toJson(executor.apply(config.get(path)))); // Json
+					components = new ArrayList<>();
+					components.add(ComponentAPI.fromJson(CustomJsonWriter.toJson(executor.apply(config.get(path))))); // Json
+					inString = convertToReadableStringForConsole(components.get(0));
 				} else if (config.get(path) instanceof Collection) {
-					List<Object> components = new ArrayList<>();
+					components = new ArrayList<>();
+					StringContainer container = new StringContainer(64);
 					for (Object value : config.getList(path))
-						if (value instanceof Collection)
-							components.add(BukkitLoader.getNmsProvider().chatBase(CustomJsonWriter.toJson(executor.apply(value)))); // Json
-						else
-							components.add(BukkitLoader.getNmsProvider().toIChatBaseComponent(ComponentAPI.fromString(executor.applyAfterColorize(value.toString()))));
-					chatBase = components;
+						if (value instanceof Collection) {
+							Component component = ComponentAPI.fromJson(CustomJsonWriter.toJson(executor.apply(value)));
+							components.add(component); // Json
+							if (!container.isEmpty())
+								container.append('\n');
+							container.append(convertToReadableStringForConsole(component));
+						} else {
+							Component component = ComponentAPI.fromString(executor.applyAfterColorize(value.toString()));
+							components.add(component); // String
+							if (!container.isEmpty())
+								container.append('\n');
+							container.append(convertToReadableStringForConsole(component));
+						}
 					if (components.isEmpty()) {
 						result.complete(null);
 						return;
 					}
+					inString = container.toString();
 					collection = true;
 				} else {
 					String value = config.getString(path);
@@ -84,11 +120,14 @@ public class MessageManager {
 						result.complete(null);
 						return;
 					}
-					chatBase = BukkitLoader.getNmsProvider().toIChatBaseComponent(ComponentAPI.fromString(executor.applyAfterColorize(value))); // String
+					components = new ArrayList<>();
+					components.add(ComponentAPI.fromString(executor.applyAfterColorize(value))); // String
+					inString = convertToReadableStringForConsole(components.get(0));
 				}
+				if (owner.hasPermission("css.chat.placeholders"))
+					replaceChatPlaceholders(API.get().getConfigManager().getChat(), components, new AtomicInteger(0), API.get().getConfigManager().getChat().getInt("placeholders.limit-per-message"));
 				if (!collection) {
-					Object packet = BukkitLoader.getNmsProvider().packetChat(ChatType.SYSTEM, chatBase);
-					String inString = BukkitLoader.getNmsProvider().fromIChatBaseComponent(chatBase).toString();
+					Object packet = BukkitLoader.getNmsProvider().packetChat(ChatType.SYSTEM, BukkitLoader.getNmsProvider().toIChatBaseComponent(components));
 					for (CommandSender player : receivers)
 						if (player instanceof Player)
 							BukkitLoader.getPacketHandler().send((Player) player, packet);
@@ -97,18 +136,15 @@ public class MessageManager {
 					result.complete(inString);
 					return;
 				}
-				StringContainer container = new StringContainer(64);
-				for (Object component : (List<?>) chatBase) {
+				for (Component component : components) {
 					Object packet = BukkitLoader.getNmsProvider().packetChat(ChatType.SYSTEM, component);
-					String inString = BukkitLoader.getNmsProvider().fromIChatBaseComponent(component).toString();
 					for (CommandSender player : receivers)
 						if (player instanceof Player)
 							BukkitLoader.getPacketHandler().send((Player) player, packet);
 						else
-							player.sendMessage(inString);
-					container.append(inString);
+							player.sendMessage(convertToReadableStringForConsole(component));
 				}
-				result.complete(container.toString());
+				result.complete(inString);
 				return;
 			}
 			if (!config.existsKey(path)) {
@@ -157,6 +193,134 @@ public class MessageManager {
 						player.sendMessage(BukkitLoader.getNmsProvider().fromIChatBaseComponent(chatBase).toString());
 			}
 		}
+
+		private String getNbtOf(ItemStack itemInHand) {
+			Object nbt = BukkitLoader.getNmsProvider().getNBT(itemInHand);
+			return nbt == null ? null : nbt + "";
+		}
+
+		private void replaceChatPlaceholders(Config config, List<Component> components, AtomicInteger totalPlaceholders, int limitPlaceholders) {
+			ListIterator<Component> itr = components.listIterator();
+			while (itr.hasNext()) {
+				Component comp = itr.next();
+				int[] find;
+				int prevPos = 0;
+				while ((find = find(prevPos, comp.getText())) != null) {
+					if (totalPlaceholders.getAndIncrement() >= limitPlaceholders)
+						break;
+					String prefix = comp.getText().substring(prevPos, find[0]);
+					String suffix = comp.getText().substring(find[0] + find[1]);
+					comp.setText(prefix);
+					prevPos = find[0] + find[1];
+					ItemStack itemInHand = owner.getItemInHand();
+					if (itemInHand.getType() != Material.AIR && find[1] == 6) {
+						String itemName = itemInHand.hasItemMeta() && itemInHand.getItemMeta().hasDisplayName() ? itemInHand.getItemMeta().getDisplayName() : null;
+						if (itemName == null) {
+							StringContainer container = new StringContainer(itemInHand.getType().name().length());
+							boolean first = true;
+							for (String split : itemInHand.getType().name().split("_")) {
+								if (first) {
+									container.append(split.charAt(0)).append(split.substring(1).toLowerCase());
+									first = false;
+									continue;
+								}
+								container.append(' ');
+								if (split.equals("OF") || split.equals("THE"))
+									container.append(split.toLowerCase());
+								else
+									container.append(split.charAt(0)).append(split.substring(1).toLowerCase());
+							}
+							itemName = container.toString();
+						}
+						String id = UUID.randomUUID().toString();
+						GUI inv = new GUI(config.getString("placeholders.item.inv-title").replace("{player}", owner.getName()), 27);
+						for (int i = 0; i < 27; ++i)
+							if (i == 13)
+								inv.getInventory().setItem(i, itemInHand);
+							else
+								inv.getInventory().setItem(i, EMPTY);
+						synchronized (PreCommandListener.guis) {
+							PreCommandListener.guis.put(id, inv);
+						}
+						String value = ColorUtils.colorize(config.getString("placeholders.item.replace"), Arrays.asList("{itemName}", "{player}")).replace("{player}", owner.getName())
+								.replace("{itemName}", itemName);
+						Component item = ComponentAPI.fromString(value);
+						item.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/css-openinv " + id));
+						item.setHoverEvent(
+								new HoverEvent(HoverEvent.Action.SHOW_ITEM, new ComponentItem(itemInHand.getType().name().toLowerCase(), itemInHand.getAmount()).setNbt(getNbtOf(itemInHand))));
+						if (item.getExtra() != null)
+							for (Component extra : item.getExtra()) {
+								extra.setClickEvent(item.getClickEvent());
+								extra.setHoverEvent(item.getHoverEvent());
+							}
+						itr.add(item);
+					} else if (find[1] == 5) {
+						String id = UUID.randomUUID().toString();
+						GUI inv = new GUI(config.getString("placeholders.inventory.inv-title").replace("{player}", owner.getName()), 36);
+						inv.getInventory().setStorageContents(owner.getInventory().getStorageContents());
+						synchronized (PreCommandListener.guis) {
+							PreCommandListener.guis.put(id, inv);
+						}
+
+						String value = ColorUtils.colorize(config.getString("placeholders.inventory.replace").replace("{player}", owner.getName()));
+						Component item = ComponentAPI.fromString(value);
+						item.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/css-openinv " + id));
+						if (!config.getString("placeholders.inventory.hoverEvent").isEmpty())
+							item.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+									ComponentAPI.fromString(ColorUtils.colorize(config.getString("placeholders.inventory.hoverEvent").replace("{player}", owner.getName())), true, false)));
+						if (item.getExtra() != null)
+							for (Component extra : item.getExtra()) {
+								extra.setClickEvent(item.getClickEvent());
+								extra.setHoverEvent(item.getHoverEvent());
+							}
+						itr.add(item);
+					} else if (find[1] == 4) {
+						String id = UUID.randomUUID().toString();
+						GUI inv = new GUI(config.getString("placeholders.enderchest.inv-title").replace("{player}", owner.getName()), owner.getEnderChest().getSize());
+						inv.getInventory().setStorageContents(owner.getEnderChest().getStorageContents());
+						synchronized (PreCommandListener.guis) {
+							PreCommandListener.guis.put(id, inv);
+						}
+						String value = ColorUtils.colorize(config.getString("placeholders.enderchest.replace").replace("{player}", owner.getName()));
+						Component item = ComponentAPI.fromString(value);
+						if (!config.getString("placeholders.enderchest.hoverEvent").isEmpty())
+							item.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+									ComponentAPI.fromString(ColorUtils.colorize(config.getString("placeholders.enderchest.hoverEvent").replace("{player}", owner.getName())), true, false)));
+						item.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/css-openinv " + id));
+						if (item.getExtra() != null)
+							for (Component extra : item.getExtra()) {
+								extra.setClickEvent(item.getClickEvent());
+								extra.setHoverEvent(item.getHoverEvent());
+							}
+						itr.add(item);
+					}
+					itr.add(new Component(suffix).copyOf(comp).setClickEvent(comp.getClickEvent()).setHoverEvent(comp.getHoverEvent()).setInsertion(comp.getInsertion()));
+					itr.previous();
+				}
+			}
+
+			for (Component component : components)
+				if (component.getExtra() != null)
+					replaceChatPlaceholders(config, component.getExtra(), totalPlaceholders, limitPlaceholders);
+		}
+
+		private int[] find(int startAt, String value) {
+			for (int i = startAt; i < value.length(); ++i) {
+				char c = value.charAt(i);
+				if (c == '[') {
+					if (value.length() > i + 5)
+						if (value.charAt(i + 1) == 'i' && value.charAt(i + 2) == 't' && value.charAt(i + 3) == 'e' && value.charAt(i + 4) == 'm' && value.charAt(i + 5) == ']')
+							return new int[] { i, 6 };
+					if (value.length() > i + 4)
+						if (value.charAt(i + 1) == 'i' && value.charAt(i + 2) == 'n' && value.charAt(i + 3) == 'v' && value.charAt(i + 4) == ']')
+							return new int[] { i, 5 };
+					if (value.length() > i + 3)
+						if (value.charAt(i + 1) == 'e' && value.charAt(i + 2) == 'c' && value.charAt(i + 3) == ']')
+							return new int[] { i, 4 };
+				}
+			}
+			return null;
+		}
 	}
 
 	static List<Action> actions = Collections.synchronizedList(new ArrayList<>());
@@ -174,6 +338,72 @@ public class MessageManager {
 		}.runRepeating(1, 1);
 	}
 
+	public String convertToReadableStringForConsole(Component component) {
+		StringContainer builder = new StringContainer(component.getText().length() + 8);
+
+		String colorBefore = null;
+
+		// COLOR
+		if (component.getColor() != null) {
+			if (component.getColor().charAt(0) == '#')
+				colorBefore = hexToReadableFormat(component.getColor());
+			else
+				colorBefore = "§" + component.colorToChar();
+			builder.append(colorBefore);
+		}
+
+		// FORMATS
+		String formatsBefore = component.getFormats();
+		builder.append(formatsBefore);
+
+		builder.append(component.getText());
+
+		if (component.getExtra() != null)
+			for (Component c : component.getExtra()) {
+				toString(c, builder, colorBefore, formatsBefore);
+				if (c.getColor() != null)
+					if (c.getColor().charAt(0) == '#')
+						colorBefore = hexToReadableFormat(c.getColor());
+					else
+						colorBefore = "§" + c.colorToChar();
+				String formats = c.getFormats();
+				if (!formats.isEmpty())
+					formatsBefore = formats;
+			}
+		return builder.toString();
+	}
+
+	// Deeper toString with "anti" copying of colors & formats
+	private void toString(Component component, StringContainer builder, String parentColorBefore, String parentFormatsBefore) {
+		String colorBefore = parentColorBefore;
+
+		// FORMATS
+		String formatsBefore = component.getFormats();
+		// COLOR
+		if (component.getColor() != null) {
+			if (component.getColor().charAt(0) == '#')
+				colorBefore = hexToReadableFormat(component.getColor());
+			else
+				colorBefore = "§" + component.colorToChar();
+			if (!colorBefore.equals(parentColorBefore) || !formatsBefore.equals(parentFormatsBefore))
+				builder.append(colorBefore);
+		}
+
+		// FORMATS
+		if (!formatsBefore.equals(parentFormatsBefore))
+			builder.append(formatsBefore);
+
+		builder.append(component.getText());
+
+		if (component.getExtra() != null)
+			for (Component c : component.getExtra())
+				toString(c, builder, colorBefore, formatsBefore);
+	}
+
+	private String hexToReadableFormat(String color) {
+		return new String(new char[] { '§', 'x', '§', color.charAt(1), '§', color.charAt(2), '§', color.charAt(3), '§', color.charAt(4), '§', color.charAt(5), '§', color.charAt(6) });
+	}
+
 	public void sendMessageFromFile(Config transFile, String pathToTranslation, PlaceholdersExecutor ex, Collection<? extends CommandSender> receivers) {
 		actions.add(new Action(transFile, pathToTranslation, ex, receivers.toArray(new CommandSender[0])));
 	}
@@ -182,9 +412,9 @@ public class MessageManager {
 		actions.add(new Action(transFile, pathToTranslation, ex, receivers));
 	}
 
-	public String sendMessageFromFileWithResult(Config transFile, String pathToTranslation, PlaceholdersExecutor ex, Collection<? extends CommandSender> receivers) {
+	public String sendMessageFromFileWithResult(Config transFile, String pathToTranslation, PlaceholdersExecutor ex, Collection<? extends CommandSender> receivers, Player player) {
 		CompletableFuture<String> future = new CompletableFuture<>();
-		actions.add(0, new Action(transFile, pathToTranslation, ex, receivers.toArray(new CommandSender[0]), future)); // Inserts on first priority
+		actions.add(0, new Action(transFile, pathToTranslation, ex, receivers.toArray(new CommandSender[0]), future, player)); // Inserts on first priority
 		try {
 			return future.get(); // Freeze current thread and await result from another thread
 		} catch (Exception e) {
