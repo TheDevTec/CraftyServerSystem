42db175b3e37787c89b834312fe309b3b3b1b455
Straikerinos <straiker123@email.cz>
2025-04-02 20:09:40 +0200

    Show fake number of online players (don't count vanished players)

--- src/main/java/me/devtec/craftyserversystem/utils/InternalPlaceholders.java
+++ src/main/java/me/devtec/craftyserversystem/utils/InternalPlaceholders.java
@@ -14,8 +14,8 @@ public class InternalPlaceholders {
 		Location loc = player.getLocation();
 		return PlaceholdersExecutor.i().papi(player.getUniqueId()).add("player", player.getName())
 				.add("tps", BukkitLoader.getNmsProvider().getServerTPS()[0])
-				.add("ping", BukkitLoader.getNmsProvider().getPing(player))
-				.add("online", BukkitLoader.getOnlinePlayers().size()).add("max_players", Bukkit.getMaxPlayers())
+				.add("ping", BukkitLoader.getNmsProvider().getPing(player)).add("online", countNonVanishPlayers(player))
+				.add("max_players", Bukkit.getMaxPlayers())
 				.add("balance",
 						API.get().getEconomyHook().format(
 								API.get().getEconomyHook().getBalance(player.getName(), player.getWorld().getName())))
@@ -25,5 +25,13 @@ public class InternalPlaceholders {
 				.add("health", player.getHealth()).add("food", player.getFoodLevel()).add("x", loc.getX())
 				.add("y", loc.getY()).add("z", loc.getZ()).add("pos_x", loc.getBlockX()).add("pos_y", loc.getBlockY())
 				.add("pos_z", loc.getBlockZ()).add("world", loc.getWorld().getName());
+	}
+
+	private static int countNonVanishPlayers(Player player) {
+		int count = 0;
+		for (Player online : BukkitLoader.getOnlinePlayers())
+			if (online.equals(player) || player.canSee(online))
+				++count;
+		return count;
 	}
 }
